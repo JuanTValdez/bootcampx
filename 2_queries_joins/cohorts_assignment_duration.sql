43e4a0df9dfaecb3ff7fc9ee5200d2b7f9fd2ec5
Juan <juantvaldez85.@gmail.com>
2021-10-05 23:41:12 +0000

    add group by queries

--- 2_queries_joins/cohorts_assignment_duration.sql
+++ 2_queries_joins/cohorts_assignment_duration.sql
@@ -4,3 +4,9 @@ JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_idff
 WHERE cohorts.name = 'FEB12';
 
+-- SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions 
+-- JOIN students ON students.id = student_id
+-- WHERE students.end_date IS null
+-- HAVING student.
+-- GROUP BY students.name;
