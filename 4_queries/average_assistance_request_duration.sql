b40b3e9f0d37050524955184cf8bf095e97f579c
Juan <juantvaldez85.@gmail.com>
2021-10-06 03:35:20 +0000

    add avg assist request duration

---
+++ 4_queries/average_assistance_request_duration.sql
@@ -0,0 +1,16 @@
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
+
+
+SELECT avg(completed_at-started_at) as average_assistance_request_duration FROM assistance_requests;
